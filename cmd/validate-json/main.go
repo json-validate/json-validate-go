99f578923f9d65228bca520695da6527d79571f8
Ulysse Carion <ulyssecarion@gmail.com>
2019-02-25 22:44:10 -0800

    Add working validate-json command

--- cmd/validate-json/main.go
+++ cmd/validate-json/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"os"
 
+	"github.com/json-validate/json-pointer-go"
+	jsonvalidate "github.com/json-validate/json-validate-go"
 	"github.com/urfave/cli"
 )
 
@@ -33,6 +36,13 @@ EXAMPLE:
 		 The order of the arguments in the two examples above does not matter.
 `
 
+type outputFormat int
+
+const (
+	outputFormatString outputFormat = iota + 1
+	outputFormatJSON
+)
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "validate-json"
@@ -48,17 +58,108 @@ func main() {
 			Name:  "schema-uri, u",
 			Usage: "the URI of the schema to validate against",
 		},
+		cli.StringFlag{
+			Name:  "format, f",
+			Usage: "how to format validation errors",
+			Value: "string",
+		},
 	}
 
 	app.CustomAppHelpTemplate = cli.AppHelpTemplate + exampleMessage
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("hello, world")
-		return nil
+		format := outputFormatString
+
+		switch c.String("format") {
+		case "string":
+			format = outputFormatString
+		case "json":
+			format = outputFormatJSON
+		default:
+			return fmt.Errorf("unknown format: %s", c.String("format"))
+		}
+
+		return run(c.Args(), format)
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
+}
+
+func run(schemaPaths []string, format outputFormat) error {
+	// parse each of the inputted paths into Schema structs
+	schemas := make([]jsonvalidate.Schema, len(schemaPaths))
+	for i, schemaPath := range schemaPaths {
+		reader, err := os.Open(schemaPath)
+		if err != nil {
+			return err
+		}
+
+		decoder := json.NewDecoder(reader)
+		err = decoder.Decode(&schemas[i])
+		if err != nil {
+			return err
+		}
+	}
+
+	// construct a new validator from the given schemas
+	validator, err := jsonvalidate.NewValidator(schemas)
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(os.Stdin)  // parses JSON from stdin
+	encoder := json.NewEncoder(os.Stdout) // outputs JSON to stdout (for json output format)
+
+	// i keeps track of which instance we're evaluating
+	for i := 0; true; i++ {
+		// read a JSON value out of stdin
+		var instance interface{}
+		err := decoder.Decode(&instance)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+
+			return err
+		}
+
+		// validate the parsed JSON value
+		result, err := validator.Validate(instance)
+		if err != nil {
+			return err
+		}
+
+		// output the errors
+		for _, vErr := range result.Errors {
+			switch format {
+			case outputFormatString:
+				fmt.Printf(
+					"%d: error at: %#v (due to %#v) (schema id: %#v)\n",
+					i, vErr.InstancePath.String(), vErr.SchemaPath.String(), vErr.SchemaURI.String(),
+				)
+			case outputFormatJSON:
+				out := struct {
+					Instance     int             `json:"instance"`
+					InstancePath jsonpointer.Ptr `json:"instancePath"`
+					SchemaPath   jsonpointer.Ptr `json:"schemaPath"`
+					SchemaURI    string          `json:"schemaURI"`
+				}{
+					i,
+					vErr.InstancePath,
+					vErr.SchemaPath,
+					vErr.SchemaURI.String(),
+				}
+
+				err := encoder.Encode(out)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return err
 }
